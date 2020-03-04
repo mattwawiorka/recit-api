92bfbf868128852e3bc66ad79e864175c3be1a2d
mattwawiorka <mattwawiorka@gmail.com>
2020-03-03 21:08:40 -0800

    updated sports list

--- util/lists.js
+++ util/lists.js
@@ -1,15 +1,15 @@
 // SPORTS LIST
 
 const SPORT = [
+    { value: "BADMINTON", label: "Badminton"},
     { value: "BASKETBALL", label: "Basketball"},
     { value: "DISCGOLF", label: "Disc Golf"},
     { value: "FOOTBALL", label: "Football"},
-    { value: "KICKBALL", label: "Kickball"},
-    { value: "SKATEBOARDING", label: "Skateboarding"},
     { value: "SOCCER", label: "Soccer"},
     { value: "SOFTBALL", label: "Softball"},
     { value: "TABLETENNIS", label: "Table Tennis"},
     { value: "TENNIS", label: "Tennis"},
+    { value: "ULTIMATEFRISBEE", label: "Ultimate Frisbee"},
     { value: "VOLLEYBALL", label: "Volleyball"},
     { value: "OTHER", label: "Other"},
 ];
